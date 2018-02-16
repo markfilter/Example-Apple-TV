04ea9cf894468c0af2d089af1c91e5e58ab19d97
Mark Filter <markfilter@outlook.com>
2018-02-16 18:59:47 -0500

    Initial Commit

---
+++ WeatherStation/WeatherStation/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  WeatherStation
+//
+//  Created by Mark Filter on 2/16/18.
+//  Copyright © 2018 Mark Filter. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
